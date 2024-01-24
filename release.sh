42ed8963c474d379ba5f01cddea4114f737918d3
Nuwan Alawatta <nala5539@sysco.com>
2024-01-24 17:59:01 +0530

    ci: ci 3

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 VERSION=$1
-NOTES=$2
 
 git checkout -b release/$VERSION
 git add package.json CHANGELOG.md
-git commit -m "chore(release): $VERSION [skip ci]\n\n${NOTES}"
+git commit -m "chore(release): $VERSION [skip ci]"
 git push origin release/$VERSION
 
-gh pr create --base main --head release/$VERSION --title "Release $VERSION" --body "$NOTES"
+gh pr create --base main --head release/$VERSION --title "Release $VERSION" --body "Release $VERSION PR"
+
