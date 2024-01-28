6a03a16a9dcbdd88364a37ef10dffb18aaea9d0e
Nuwan Alawatta <nala5539@sysco.com>
2024-01-29 00:39:19 +0530

    fix(comp): fix issues

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ git add package.json CHANGELOG.md
 git commit -m "chore(release): $VERSION [skip ci]"
 git push origin release/$VERSION
 
-gh pr create --base main --head release/$VERSION --title "Release $VERSION" --body "Release $VERSION PR"
+gh pr create --base main --head release/$VERSION --title "Release $VERSION" --body "Update files related to release $VERSION"
 
