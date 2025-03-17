4b5cb51a4a1fea063b00780ce7843449bfc234bf
Nuwan Alawatta <nala5539@sysco.com>
2025-03-17 23:06:34 +0530

    fix: format

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 //index.js
 
-export function add(a, b) {
-  return a + b;
-}
-export function divide(a, b) {
-  return a/b;
+export function add (a, b) {
+  return a + b
 }
 
-export function multiply(a, b) {
-    return a*b;
+export function divide (a, b) {
+  return a / b
 }
 
-export function subtract(a, b) {
-    return a-b;
+export function multiply (a, b) {
+  return a * b
 }
 
-export function square(a) {
-    return a*a;
+export function subtract (a, b) {
+  return a - b
+}
+
+export function square (a) {
+  return a * a
 }
